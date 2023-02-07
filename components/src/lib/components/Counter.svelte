02a3f047e64de432bed764d84fd845b339f4c114
Mike Shearer <shearerbeard@gmail.com>
2023-02-07 12:13:40 -0700

    init commit

---
+++ components/src/lib/components/Counter.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let initial: number;
+
+    let count = initial;
+
+    const increment = () => count++;
+</script>
+
+<div>
+    <p>Your number is { count }</p>
+    <button on:click={ increment }>Increment</button>
+</div>
