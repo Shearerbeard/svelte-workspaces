10f75356b973dd635584e1bca0eb1f56b4109cc7
Mike Shearer <shearerbeard@gmail.com>
2023-02-08 09:33:55 -0700

    import using npm workspace package and import using svelte alias

--- site-b/src/routes/+page.svelte
+++ site-b/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+	import Counter from '$components/Counter.svelte';
+</script>
+
+<h1>Welcome to SvelteKit - Site B</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Counter initial={ 42 } />
